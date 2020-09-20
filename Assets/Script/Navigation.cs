cb2b7f9fbb639f54610f2421d879910a5ecf43ca
Jarqin <irfan.fajarmuttaqin@gmail.com>
2020-09-20 14:54:42 +0700

    First Commit

---
+++ Assets/Script/Navigation.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Navigation : MonoBehaviour
+{
+ 	public void Retry()
+ 	{
+ 		Application.LoadLevel(Application.Loadedlevel);
+ 	}
+
+ 	public void Keluar()
+ 	{
+ 		Application.LoadLevel(Application.Loadedlevel);
+ 	}
+}
---
+++ Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    Rigidbody rb;
+	public float multiplier;
+	public Text textScore;
+	int score;
+    
+    void Start()
+    {
+    	rb = GetComponent<Rigidbody>();
+    }
+
+    
+    void Update()
+    {
+    	float moveX = Input.acceleration.x;
+    	float moveY = Input.acceleration.y;
+        
+        Vector3 posisi = new Vector3(moveX,0f,moveY);
+
+        rb.AddForce(posisi*multiplier);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+    	if (other.gameObject.tag == "Kubus")
+    	{
+    		Destroy(other.gameObject);
+    		score++;
+
+    		textScore.text = "Score :" + score;
+    	}
+	}
+}
